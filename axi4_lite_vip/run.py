08bc89bd851ad5da905c7c0e643acef98a0cbb29
yangyt96 <yeeyang.tan@live.com>
2026-04-24 17:22:34 +0800

    [test] add wcfg

--- axi4_lite_vip/run.py
+++ axi4_lite_vip/run.py
@@ -19,4 +19,9 @@ lib.add_source_files(
     include_dirs=[(ROOT / "sim").as_posix()],
 )
 
+lib.set_sim_option(
+    name="modelsim.init_file.gui",
+    value=str(ROOT / "tb/axi4_lite_vip_tb.do"),
+)
+
 vu.main()
